6643bc76a71a8c99aeef88ad7496651df88c4f45
BHMS2022 <108097125+BHMS2022@users.noreply.github.com>
2023-03-02 19:14:52 -0800

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,12 @@
+
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(0, 0, 300, 300);
+    canvas.center();
+}
+function draw(){
+}
+function takeSnapshot(){
+    save("filtered_image.png");
+}
